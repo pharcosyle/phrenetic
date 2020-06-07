711057d7522a590804ebd58bc6ac5f47b3a0649c
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-06-06 19:17:31 -0700

    Added gdscript to init.el

--- init.el
+++ init.el
@@ -123,6 +123,7 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
+       ;;gdscript          ; the language you waited for
        ;;go                ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
