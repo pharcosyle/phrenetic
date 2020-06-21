4ec619f0e6ea2ceb3b77a7258902b1f3e520355e
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-06-21 07:55:50 -0700

    Bump dash

--- packages.el
+++ packages.el
@@ -9,6 +9,6 @@
                    sws-mode)
 
 ;; (package! aggressive-indent :pin "12a64b4e5c1a1e124baa74336738b6ae1972607f")
-(package! dash :pin "ea4a4cc7cce7c3b93862a22df8bca8b83052ccbf")
+(package! dash :pin "732d92eac56023a4fb4a5dc3d9d4e274ebf44bf9")
 (package! graphql-mode :pin "7c37aee28bf8c8ffb3da73df5571c4e1e352562b")
 (package! tldr :pin "269bda7001613c0b70c0662d2a74d200765c1dcb")
