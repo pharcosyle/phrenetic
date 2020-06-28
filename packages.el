153cb82ff35ff2f160aa1809684b8a3fa6833387
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-06-27 19:26:11 -0700

    Don't `disable-packages!`
    Keep them around just in case I encounter some new code where they're
    useful (phuc endeavors are likely to use many different technologies).

--- packages.el
+++ packages.el
@@ -1,13 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(disable-packages! emmet-mode
-                   haml-mode
-                   pug-mode
-                   slim-mode
-                   stylus-mode
-                   sws-mode)
-
 ;; (package! aggressive-indent :pin "12a64b4e5c1a1e124baa74336738b6ae1972607f")
 (package! dash :pin "732d92eac56023a4fb4a5dc3d9d4e274ebf44bf9")
 (package! graphql-mode :pin "7c37aee28bf8c8ffb3da73df5571c4e1e352562b")
