997490bd8aa603e0fb256f3329fd58e306cec09c
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-08-26 19:31:00 -0700

    Bump graphql-mode

--- packages.el
+++ packages.el
@@ -3,5 +3,5 @@
 
 ;; (package! aggressive-indent :pin "12a64b4e5c1a1e124baa74336738b6ae1972607f")
 (package! dash :pin "b92ab5a39b987e4fe69317b9d9fda452300baf20")
-(package! graphql-mode :pin "7c37aee28bf8c8ffb3da73df5571c4e1e352562b")
+(package! graphql-mode :pin "9bed568ec86242dbe30bdbab324aa0eb2cd9bf08")
 (package! tldr :pin "269bda7001613c0b70c0662d2a74d200765c1dcb")
