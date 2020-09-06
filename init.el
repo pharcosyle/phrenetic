1412f99b1176d8e01b5dc1a38c39f05539c784dd
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-09-06 10:19:34 -0700

    Add `magit +forge` module and `pass +auth` to authenticate it.

--- init.el
+++ init.el
@@ -94,9 +94,9 @@
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        ;;lsp
-       magit             ; a git porcelain for Emacs
+       (magit +forge)      ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
-       pass              ; password manager for nerds
+       (pass +auth)        ; password manager for nerds
        ;;pdf               ; pdf enhancements
        prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
