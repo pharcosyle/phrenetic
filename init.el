27e13685c3588f87e267b4118db6bb9ba0024d6d
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-09-07 10:54:44 -0700

    Trying out popup module with +all

--- init.el
+++ init.el
@@ -41,7 +41,8 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
-        +defaults)       ; default popup rules
+        +defaults        ; default popup rules
+        +all)
        ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
