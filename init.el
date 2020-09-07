21042b4e932817527b14f364214d162a20ddbdef
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-09-07 11:50:27 -0700

    Trying out ivy with +prescient

--- init.el
+++ init.el
@@ -23,7 +23,9 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +icons)      ; a search engine for love and life
+       (ivy              ; a search engine for love and life
+        +icons
+        +prescient)
 
        :ui
        ;;deft              ; notational velocity for Emacs
