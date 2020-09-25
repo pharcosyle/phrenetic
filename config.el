8b40f8acad7f30d5f852d527bab05b22e5d3a2e3
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-09-25 05:14:09 -0700

    Trying out expand-region

--- config.el
+++ config.el
@@ -207,6 +207,18 @@
 ;;   (setq undo-tree-visualizer-timestamps t))
 
 
+(use-package! expand-region
+  :commands er/expand-region
+  :init
+  (map! "s-C-i" #'er/expand-region
+        "s-C-o" #'er/contract-region)
+  :config
+  ;; Copied from doom config (with, at the time of this writing, one modification): ~/.emacs.d/modules/config/default/+emacs.el::12
+  (defadvice! my--quit-expand-region-a ()
+    "Properly abort an expand-region region."
+    :before '(evil-escape doom/escape evil-force-normal-state)
+    (when (memq last-command '(er/expand-region er/contract-region))
+      (er/contract-region 0))))
 
 (use-package! ivy-avy
   :after ivy)
--- packages.el
+++ packages.el
@@ -2,6 +2,7 @@
 ;;; $DOOMDIR/packages.el
 
 (package! dash :pin "b92ab5a39b987e4fe69317b9d9fda452300baf20")
+(package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781")
 (package! graphql-mode :pin "9bed568ec86242dbe30bdbab324aa0eb2cd9bf08")
 (package! ivy-avy :pin "7e4c56776f811f78b8eb95210156f8fbbdba67e7")
 (package! tldr :pin "269bda7001613c0b70c0662d2a74d200765c1dcb")
