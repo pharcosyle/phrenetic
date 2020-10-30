a1ef8b94b4d9feeda118cf25412b8569527bfb21
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-10-29 23:08:08 -0700

    Bump packages

--- packages.el
+++ packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! dash :pin "b92ab5a39b987e4fe69317b9d9fda452300baf20")
+(package! dash :pin "0f238a9a466879ee96e5db0482019453718f342d")
 (package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781")
-(package! graphql-mode :pin "9bed568ec86242dbe30bdbab324aa0eb2cd9bf08")
-(package! ivy-avy :pin "7e4c56776f811f78b8eb95210156f8fbbdba67e7")
+(package! graphql-mode :pin "2371316a750b807de941184d49ca19d277ecadcd")
+(package! ivy-avy :pin "b472d347f6a3d31afe2807c0c0bb76a8ab128efe")
 (package! tldr :pin "269bda7001613c0b70c0662d2a74d200765c1dcb")
