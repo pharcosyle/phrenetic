9823384fd0f6a954ee142ce7be4b27baee72a01b
Krzysztof Baranowski <pharcosyle@gmail.com>
2020-11-25 19:24:40 -0800

    Revert "ivy-avy is part of the swiper repo and it seems like Doom package"
    This reverts commit a56dde842aab2c93364659274fc334f51fb8813f.

--- packages.el
+++ packages.el
@@ -4,4 +4,5 @@
 (package! dash :pin "0f238a9a466879ee96e5db0482019453718f342d")
 (package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781")
 (package! graphql-mode :pin "2371316a750b807de941184d49ca19d277ecadcd")
+(package! ivy-avy :pin "6a22192f11a4aa0d43692bf685fd9b9d75911d00")
 (package! tldr :pin "269bda7001613c0b70c0662d2a74d200765c1dcb")
