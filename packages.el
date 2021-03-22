be72826d65b037816162e387f15b5d6000bdba47
Krzysztof Baranowski <pharcosyle@gmail.com>
2021-03-21 17:15:48 -0700

    Bump packages

--- packages.el
+++ packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! dash :pin "e0254c56e78b82f670e2d92ad7077c6cc96db1cf")
-(package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781")
+(package! dash :pin "a17b6b5409825891423b3867cd7bea84852d6ddd")
+(package! expand-region :pin "4b8322774d9c1d8b64a0049d1dbbc1e7ce80c1a0")
 (package! graphql-mode :pin "2371316a750b807de941184d49ca19d277ecadcd")
-(package! tldr :pin "269bda7001613c0b70c0662d2a74d200765c1dcb")
+(package! tldr :pin "d59405bd72f3379417b9e73f06e8848b43cb021d")
