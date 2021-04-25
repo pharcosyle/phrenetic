450e3cc20b5254ae9a17581b93fd43ae009bd9dd
Krzysztof Baranowski <pharcosyle@gmail.com>
2021-04-25 15:36:57 -0700

    comment

--- config.el
+++ config.el
@@ -324,7 +324,7 @@
 (set-frame-position (selected-frame) 80 0)
 
 ;; REVIEW Do this instead once I'm not working on a broken screen. I'm concerned the `fullboth' behavior I want is specific to `emacs-mac' which I won't be using much longer.
-;; (add-to-list 'initial-frame-alist '(fullscreen . fullboth)) ; Make Emacs fill the screen without creating a new Mac space.
+;; (add-to-list 'initial-frame-alist '(fullscreen . fullboth)) ; Make Emacs fill the screen without creating a new workspace.
 
 
 
