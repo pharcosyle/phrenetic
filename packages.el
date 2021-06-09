4877e92910d8933971a1d485f627e5c3c5d64202
Krzysztof Baranowski <pharcosyle@gmail.com>
2021-06-08 18:28:15 -0700

    Bump packages (dash and guix)

--- packages.el
+++ packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! dash :pin "b9286a84975874b10493f1cb4ea051c501f51273")
+(package! dash :pin "aab346ed9d8f0f7ea033029c9688810353052e7e")
 (package! expand-region :pin "4b8322774d9c1d8b64a0049d1dbbc1e7ce80c1a0")
 (package! graphql-mode :pin "2371316a750b807de941184d49ca19d277ecadcd")
-(package! guix :pin "8ce6d219e87c5097abff9ce6f1f5a4293cdfcb31")
+(package! guix :pin "c9aef52121b458297e70bb50f49f7276b4a8d759")
 (package! tldr :pin "d59405bd72f3379417b9e73f06e8848b43cb021d")
 (package! trashed :pin "23e782f78d9adf6b5479a01bfac90b2cfbf729fe")
