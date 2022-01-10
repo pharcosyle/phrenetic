ecd37f6c34bd8934760f05c33db41c70428e4ec5
Krzysztof Baranowski <pharcosyle@gmail.com>
2022-01-09 22:40:48 -0800

    Vertico/minibuffer config, add app-launcher/0x0 packages

--- doom-additional.el
+++ doom-additional.el
@@ -6,12 +6,12 @@
 
 ;; Temporary until I get semicolon working
 (map! (:map minibuffer-local-map
-       "s-:" #'embark-act
-       "C-c s-:" #'embark-export)
+       "C-:" #'embark-act
+       "C-c C-:" #'embark-export)
       (:leader
        "A" #'embark-dwim)
       ;; Trying out cycling (temp)
-      "s-:" #'embark-act
+      "C-:" #'embark-act
       "s-q" #'embark-cycle
       ;; Override my guix binding (temp)
       (:leader
@@ -19,3 +19,11 @@
 
 ;; If I keep this I can get rid of the "C-_" binding I have too
 (biome--trans "s-h" "C-h")
+
+;; `forward-char' in original definition is messing things up, do this for now
+;; (defun +eshell/search-history ()
+;;   (interactive)
+;;   (consult-history))
+(after! esh-mode
+  (map! :map eshell-mode-map
+        "C-s" #'consult-history))
