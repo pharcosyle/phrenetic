672f1dc433baab4bb7d31a7497f5954ac4483e6c
Krzysztof Baranowski <pharcosyle@gmail.com>
2023-01-20 01:12:19 -0800

    Do .gitignore tangled files better

--- meta.el
+++ meta.el
@@ -10,3 +10,14 @@
   (concat (when-let ((dir (meta-get-dir)))
             (concat dir "/"))
           sub-path))
+
+(defvar meta--gtf-running? nil)
+
+;; Using `org-babel-tangle-collect-blocks' to implement this slows down tangling a small but noticeable amount. It also, unfortunately, performs expansion/evalution of header args, noweb references, etc. so when called in the context of an org-tangle it's almost guaranteed to infinitely recurse, hence the dynamically-bound guard variable (another way to do this would be with `unwind-protect').
+(defun meta--get-tangled-files ()
+  (when (not meta--gtf-running?)
+    (let ((meta--gtf-running? 't))
+      (mapcar #'car (org-babel-tangle-collect-blocks)))))
+
+(defun meta-tangled-files->gitignore ()
+  (string-join (meta--get-tangled-files) "\n"))
